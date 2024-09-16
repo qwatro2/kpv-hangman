c4fa6688ac3022d71084921e442bd800392804dc
qwatro2 <kv22828@gmail.com>
2024-09-16 16:10:26 +0300

    style: remove unused imports from WordTest.java

--- src/test/java/backend/academy/kernel/words/WordTest.java
+++ src/test/java/backend/academy/kernel/words/WordTest.java
@@ -1,15 +1,10 @@
 package backend.academy.kernel.words;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyChar;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class WordTest {
     private static Stream<Arguments> provideWordsAndLettersForContainsLetter() {
