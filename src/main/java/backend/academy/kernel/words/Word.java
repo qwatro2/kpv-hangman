f1b37c3bfb231934df47c17787804192d953841e
qwatro2 <kv22828@gmail.com>
2024-09-16 19:09:40 +0300

    feat: add method getIndexesOfLetter to Word

--- src/main/java/backend/academy/kernel/words/Word.java
+++ src/main/java/backend/academy/kernel/words/Word.java
@@ -1,10 +1,23 @@
 package backend.academy.kernel.words;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public record Word(String word,
                    String prompt,
                    DifficultyLevel difficultyLevel,
                    Category category) {
     public boolean containsLetter(char letter) {
         return this.word.contains(Character.toString(letter));
+    }
+
+    public List<Integer> getIndexesOfLetter(char letter) {
+        List<Integer> indexesOfLetter = new ArrayList<>();
+        for (int i = 0; i < word.length(); ++i) {
+            if (word.charAt(i) == letter) {
+                indexesOfLetter.add(i);
+            }
+        }
+        return indexesOfLetter;
     }
 }
