edbc75d798e50b2cf350661b91375d4758058570
qwatro2 <kv22828@gmail.com>
2024-09-17 01:53:33 +0300

    feat: add game initialization and running to main

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,20 @@
 package backend.academy;
 
+import backend.academy.kernel.client.ConsoleHangmanReader;
+import backend.academy.kernel.client.ConsoleHangmanWriter;
+import backend.academy.kernel.hangman.SoloHangman;
+import backend.academy.kernel.hangman.Hangman;
+import backend.academy.kernel.words.JsonWordsReceiver;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        Hangman game = new SoloHangman(
+            new JsonWordsReceiver("src/main/resources/words.json"),
+            new ConsoleHangmanReader(),
+            new ConsoleHangmanWriter()
+        );
+        game.run();
     }
 }
